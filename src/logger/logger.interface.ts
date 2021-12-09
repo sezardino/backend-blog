59a97029fc293ce3424997ce38de84de2f28b64b
Edward Arechwa <e.arechwa@gmail.com>
2021-12-09 21:00:53 +0100

    add logger

---
+++ src/logger/logger.interface.ts
@@ -0,0 +1,7 @@
+export interface ILogger {
+  info: (...args: unknown[]) => void;
+  warn: (...args: unknown[]) => void;
+  error: (...args: unknown[]) => void;
+  debug: (...args: unknown[]) => void;
+  fatal: (err: Error) => void;
+}
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,35 @@
+import { Logger } from "tslog";
+import { ILogger } from "./logger.interface";
+
+export class LoggerService implements ILogger {
+  private logger: Logger;
+
+  constructor() {
+    this.logger = new Logger({
+      displayInstanceName: false,
+      displayLoggerName: false,
+      displayFilePath: "hidden",
+      displayFunctionName: false,
+    });
+  }
+
+  debug(...args: unknown[]): void {
+    this.logger.debug(...args);
+  }
+
+  error(...args: unknown[]): void {
+    this.logger.error(...args);
+  }
+
+  fatal(err: Error): void {
+    this.logger.fatal(err);
+  }
+
+  info(...args: unknown[]): void {
+    this.logger.info(...args);
+  }
+
+  warn(...args: unknown[]): void {
+    this.logger.warn(...args);
+  }
+}
